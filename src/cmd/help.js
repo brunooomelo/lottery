b5b346683efcda12049700b2d6362b4a1d7e486a
brunooomelo <utibmelo@gmail.com>
2019-01-08 01:52:33 -0200

    arquivo help da CLI

---
+++ src/cmd/help.js
@@ -0,0 +1,24 @@
+const menus = {
+  main: `
+    Usage: lottery <command> [flags]
+
+    where <command> is one of:
+        megasena
+
+    lottery help <command>  quick help on <command>`,
+
+  megasena:  `
+    lottery megasena <options>
+
+    --concurso, -c  .......... show contest result
+  `,
+
+}
+
+module.exports = (args) => {
+  const subCmd = args._[0] === 'help'
+    ? args._[1]
+    : args._[0]
+
+  console.log(menus[subCmd] || menus.main)
+}
