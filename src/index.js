9b5c760a41cb3f4f2e67a5df96b8e255e67a5e4d
brunooomelo <utibmelo@gmail.com>
2019-01-08 01:52:03 -0200

    criação de utilização da CLI

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const minimist = require("minimist")
+const error = require('./utils/error')
+
+module.exports = () => {
+  const args = minimist(process.argv.slice(2))
+  let cmd = args._[0] || "help"
+  if (args.version || args.v) {
+    cmd = "version"
+  }
+  if (args.help || args.h) {
+    cmd = "help"
+  }
+
+  switch (cmd) {
+    case "help":
+      require("./cmd/help")(args)
+      break
+    case "version":
+      require("./cmd/version")(args)
+      break
+    default:
+      error(`"${cmd}" is not a valid command!`, true)
+      break
+  }
+}
---
+++ src/index.test.js
@@ -0,0 +1,50 @@
+const { expect } = require('chai')
+const { exec } = require('child_process')
+
+const lottery = 'node ./bin/lottery'
+const pkg = require('../package.json')
+
+describe('Main CLI', () => {
+  it('should return version of lottery', done => {
+    exec(`${lottery} --version`, (err, stdout) => {
+      if (err) throw err
+      expect(stdout.replace('\n', '')).to.be.equal(`v${pkg.version}`)
+      done()
+    })
+  })
+
+  it('should return the description when "lottery --help"', done => {
+    exec(`${lottery} --help`, (err, stdout) => {
+      if (err) throw err
+      expect(stdout.includes('Usage: lottery <command> [flags]')).to.be.true
+      done()
+    })
+  })
+
+  it('should return the description when lottery doesn`t receive args', done => {
+    exec(`${lottery}`, (err, stdout) => {
+      if (err) throw err
+      expect(stdout.includes('where <command> is one of:')).to.be.true
+      expect(stdout.includes('lottery help <command>  quick help on <command>')).to.be.true
+      done()
+    })
+  })
+
+  it('should return the megasena option when "lottery help megasena"', done => {
+    exec(`${lottery} help megasena`, (err, stdout) => {
+      if (err) throw err
+      expect(stdout.includes('lottery megasena <options>')).to.be.true
+      expect(stdout.includes('--concurso, -c  .......... show contest result')).to.be.true
+      done()
+    })
+  })
+
+  it('should return the error if command doesn`t exist', done => {
+    exec(`${lottery} fizzbuzz`, (err, stdout) => {
+      if (err) throw err
+      expect(stdout.includes('"fizzbuzz" is not a valid command!')).to.be.true
+      done()
+    })
+  })
+})
+
