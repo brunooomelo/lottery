f734a65a6e336eb69c3973264e821e66c8058b80
Victor Ferreira <victor.araujo.ferreira@gmail.com>
2019-10-17 22:27:53 -0300

    add lotomania game

--- src/index.js
+++ src/index.js
@@ -1,34 +1,37 @@
-const minimist = require('minimist')
-const error = require('./utils/error')
+const minimist = require("minimist");
+const error = require("./utils/error");
 
 module.exports = () => {
-  const args = minimist(process.argv.slice(2))
-  let cmd = args._[0] || 'help'
+  const args = minimist(process.argv.slice(2));
+  let cmd = args._[0] || "help";
   if (args.version || args.v) {
-    cmd = 'version'
+    cmd = "version";
   }
   if (args.help || args.h) {
-    cmd = 'help'
+    cmd = "help";
   }
 
   switch (cmd) {
-    case 'help':
-      require('./cmd/help')(args)
-      break
-    case 'version':
-      require('./cmd/version')(args)
-      break
-    case 'megasena':
-      require('./cmd/megasena')(args)
-      break
-    case 'lotofacil':
-      require('./cmd/lotofacil')(args)
-      break
-    case 'quina':
-      require('./cmd/quina')(args)
-      break
+    case "help":
+      require("./cmd/help")(args);
+      break;
+    case "version":
+      require("./cmd/version")(args);
+      break;
+    case "megasena":
+      require("./cmd/megasena")(args);
+      break;
+    case "lotofacil":
+      require("./cmd/lotofacil")(args);
+      break;
+    case "quina":
+      require("./cmd/quina")(args);
+      break;
+    case "lotomania":
+      require("./cmd/lotomania")(args);
+      break;
     default:
-      error(`"${cmd}" is not a valid command!`, true)
-      break
+      error(`"${cmd}" is not a valid command!`, true);
+      break;
   }
-}
+};
